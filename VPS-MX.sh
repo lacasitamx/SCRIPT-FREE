747978465254e0d10e1ad63d91fea5c7f1b86e2e
SCRIPTS 2021 <69657855+lacasitamx@users.noreply.github.com>
2021-05-03 11:23:23 -0600

    Add files via upload

---
+++ VPS-MX.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+clear
+echo -ne '\e[97mCARGANDO:\033[1;37m\e[93m #######                        (25%)\r'
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+apt install python -y > /dev/null 2>&1
+echo -ne '\e[97mCARGANDO:\033[1;37m\e[93m ###############                (50%)\r'
+#cd /usr/bin/
+#wget https://www.dropbox.com/s/q118kiel62bovck/pytransform.tar >/dev/null 2>&1
+#tar -xf pytransform.tar > /dev/null 2>&1 
+#rm -rf pytransform.tar > /dev/null 2>&1
+#cd
+echo -ne '\e[97mCARGANDO:\033[1;37m\e[93m ######################         (75%)\r'
+wget https://www.dropbox.com/s/xp2zbqz3nzkes1i/VPS-MX.py -O /usr/bin/VPS-MX &> /dev/null
+chmod +x /usr/bin/VPS-MX
+echo -ne '\e[97mCARGANDO:\033[1;37m\e[93m ############################## (100%)\r'
+echo -ne '\n'
+sleep 2s
+VPS-MX
+rm -rf /root/VPS-MX
+exit
+
+#HOLAS CREA TU PROPIO SCRIPT PAPU
